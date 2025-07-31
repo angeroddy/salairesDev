d9890b51dff156df3de6488cbd30feb3efd51ad0
Djesse Ange Roddy N'Guessan <djesse-ange-roddy.n-guessan@etu.univ-grenoble-alpes.fr>
2025-07-31 14:49:22 +0200

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import Landing from "./pages/landing";
 import AddSalary from "./pages/addSalary";
 import ConfirmPage from "./pages/confirm";
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Route path="/ajouter-salaire" element={<AddSalary />} />
         <Route path="/confirm" element={<ConfirmPage />} />
       </Routes>
+      {/* Analytics pour Vercel */}
+      <Analytics />
     </Router>
   );
 }
